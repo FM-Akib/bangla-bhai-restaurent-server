37584d1dced377a3e70ee0e4b44e7c075c775089
FM-Akib <muntasirakib08@gmail.com>
2024-04-04 02:55:25 +0600

    added menu

--- index.js
+++ index.js
@@ -24,9 +24,19 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
-    // Send a ping to confirm a successful connection
+   
+
+    const menuCollection = client.db('BanglaBhaiDB').collection("menu");
+
+    app.get('/menu',async(req,res) => {
+        const result = await menuCollection.find().toArray();
+        res.send(result);
+    })
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
