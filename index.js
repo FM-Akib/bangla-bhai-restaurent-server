4cd7653a605332fde11c97b0a8b635ed93ff7f2c
FM-Akib <muntasirakib08@gmail.com>
2024-04-25 18:26:02 +0600

    added patch

--- index.js
+++ index.js
@@ -60,6 +60,18 @@ async function run() {
     res.send(result); 
   })
 
+  app.patch('/users/admin/:id', async(req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id)}
+    const updatedDoc = {
+      $set:{
+        role: 'admin',
+      }
+    }
+    const result = await usersCollection.updateOne(filter, updatedDoc);
+    res.send(result);
+  })
+
 
 
     //menu
