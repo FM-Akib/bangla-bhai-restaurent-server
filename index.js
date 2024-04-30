7647f3472e6dfe5dff08f6b2e6309943a52a3e20
FM-Akib <muntasirakib08@gmail.com>
2024-04-30 13:37:33 +0600

    bug solve  update Item page

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 var jwt = require('jsonwebtoken');
-
+const { ObjectId } = require('mongodb');
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qvgg1my.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -172,12 +172,22 @@ async function run() {
         const result = await menuCollection.find().toArray();
         res.send(result);
     })
-     
+   
+    
+    app.get('/menu/:id',async(req,res) => {
+      const id= req.params.id;
+      const query = {_id: (id)};
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    })
+    
     app.post('/menu',async(req,res) => {
       const item = req.body; 
       const result = await menuCollection.insertOne(item);
       res.send(result);
     })
+
+    
 
     app.delete('/menu/:id', async(req,res) => {
       const id = req.params.id;
