7e8ca786167773e932c39466cd64c3067410c53d
FM-Akib <muntasirakib08@gmail.com>
2024-05-01 22:35:54 +0600

    added payment gatway stripe

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-
+const stripe = require("stripe")('sk_test_51PBfLd2KZrTTgp2niaMJOZgNn7sHMeSCgqGgWlzjgStz5ynCHXo5mZwq66epo9HJYmC3po0Ilbg1BwtVO9rmFveR00wWSBduyb');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qvgg1my.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -243,6 +243,28 @@ async function run() {
     })
 
 
+
+
+    // server for the payment gateway Stripe
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price*100)
+
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+    
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
 
 
     
